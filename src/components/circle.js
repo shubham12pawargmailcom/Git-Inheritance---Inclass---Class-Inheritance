6d11099241c94101ffae7af57d5c1d9fb2a9977e
shubham12pawargmailcom <shubham.12pawar@gmail.com>
2020-10-30 15:07:48 +0530

    inheritance in class assignment

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+const Shape=require('./shape');
+class Circle extends Shape {
+    constructor() {
+        super();
+    }
+    calculateArea() {
+        console.log("area of circle with color:"+this.color);
+        return 3.14*5*5;
+    }
+}
+module.exports=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
 
 // export class using module.exports
+
+class Shape {
+    constructor() {
+        this.color="red";
+    }
+    drawShape() {
+        console.log("No shape is given");
+    } 
+    calculateArea() {
+        console.log("No shape No Area");
+    }
+}
+module.exports=Shape;
